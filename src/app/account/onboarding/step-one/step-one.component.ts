4c2f4a33cfffe6eba2799e28dbcb3a0256e76727
Ramesh Singh <rsingh2@katalysttech.com>
2023-11-23 22:12:03 +0530

    working on on-boarding

--- src/app/account/onboarding/step-one/step-one.component.ts
+++ src/app/account/onboarding/step-one/step-one.component.ts
@@ -6,11 +6,15 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./step-one.component.scss'],
 })
 export class StepOneComponent {
+  
   isLinear = true;
   formStep1Group: FormGroup;
   formSportGroup: FormGroup;
   formRoleGroup: FormGroup;
+  formClubGroup: FormGroup;
   formPhoneGroup: FormGroup;
+  
+  isClubSelected: boolean[] = [];
 
   sports = [
     { value: 'football', label: 'Football' },
@@ -28,11 +32,32 @@ export class StepOneComponent {
     { value: 'CAD', label: 'Club Admin' },
     { value: 'REC', label: 'Recruiter' },
   ];
+
+  clubs = [
+    { value: 'MUT', label: 'Manchester Utd' },
+    { value: 'MUW', label: 'Manchester Utd' },
+    { value: 'MUA', label: 'Manchester Utd' },
+    { value: 'MUV', label: 'Manchester Utd' },
+    { value: 'MUG', label: 'Manchester Utd' },
+  ];
+
+  categories = [
+    { value: 'U18', label: 'U18' },
+    { value: 'U17', label: 'U17' },
+    { value: 'U16', label: 'U16' },
+    { value: 'U15', label: 'U15' },
+    { value: 'U14', label: 'U14' },
+    { value: 'U13', label: 'U13' },
+    { value: 'U12', label: 'U12' },
+    { value: 'U11', label: 'U11' },
+  ];
   
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
-  ngOnit() {}
+  ngOnit() {
+    this.isClubSelected = Array(this.clubs.length).fill(false);
+  }
   createForm() {
     this.formStep1Group = this.fb.group({
       firstName: ['', Validators.required],
@@ -52,6 +77,12 @@ export class StepOneComponent {
         Validators.compose([Validators.required]),
       ],
     });
+    this.formClubGroup = this.fb.group({
+      club: [
+        '',
+        Validators.compose([Validators.required]),
+      ],
+    });
     this.formPhoneGroup = this.fb.group({
       mobile: [
         '',
@@ -59,4 +90,15 @@ export class StepOneComponent {
       ],
     });
   }
+  onRadioClick(event: Event) {
+    // Prevent the click event from propagating to the card
+    event.stopPropagation();
+    
+    // Toggle the isSelected state
+    // this.isClubSelected = !this.isClubSelected;
+  }
+  onCardClick(index: number) {
+    this.isClubSelected[index] = !this.isClubSelected[index];
+  }
+
 }
